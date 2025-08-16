9da2f0d4a10d6de4f1227db0a7bdff205f646ea3
Estevan Cantuaria <estevancantuaria@MacBook-Air-de-Estevan.local>
2025-08-16 20:59:07 -0300

    feat: adding constants annotations in create task dto

--- back/src/tasks/dtos/create_task.dto.ts
+++ back/src/tasks/dtos/create_task.dto.ts
@@ -1,38 +1,31 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { IsString, IsBoolean, MinLength, IsOptional, IsUUID } from 'class-validator';
+import { CREATE_TASK_DTO_ANNOTATIONS } from '../constants/dto_swagger_annotations';
 
 export class CreateTaskDto {
 
   @ApiProperty({
-    description: 'Título da tarefa',
-    example: 'Concluir projeto',
-    minLength: 3,
+    ...CREATE_TASK_DTO_ANNOTATIONS.TITLE,
   })
   @IsString()
   @MinLength(3)
   title: string;
 
   @ApiProperty({
-    description: 'ID do usuário responsável pela tarefa',
-    example: '123e4567-e89b-12d3-a456-426614174000',
-    format: 'uuid',
+    ...CREATE_TASK_DTO_ANNOTATIONS.USER_ID,
   })
   @IsUUID()
   user_id: string
 
   @ApiPropertyOptional({
-    description: 'Descrição detalhada da tarefa',
-    example: 'Finalizar todas as funcionalidades do sistema de tarefas',
-    required: false,
+    ...CREATE_TASK_DTO_ANNOTATIONS.DESCRIPTION,
   })
   @IsString()
   @IsOptional()
   description: string;
 
   @ApiPropertyOptional({
-    description: 'Status de conclusão da tarefa',
-    example: false,
-    default: false,
+    ...CREATE_TASK_DTO_ANNOTATIONS.COMPLETED,
   })
   @IsBoolean()
   @IsOptional()
